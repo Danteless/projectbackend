0899468854fb4a17aab14b27b7fc76084704b0ab
fedi <fadibrahem@hotmail.fr>
2021-07-26 00:16:18 +0100

    vv

---
+++ src/main/java/com/example/demo/models/Produit.java
@@ -0,0 +1,34 @@
+package com.example.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class Produit implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Nom;
+    private Integer QT;
+    private boolean Disponible;
+    @CreationTimestamp
+    private Timestamp date_cre;
+    private Timestamp date_mod =null;
+    private String photoName="unknown.jpg";
+    @ManyToOne()
+    @JoinColumn(name="idCategorie")
+    private Categories categorie;
+
+    public Produit(long l, String test2, int i, boolean b, Timestamp t, Object o, String s) {
+    }
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import com.example.demo.models.Produit;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.models.Categories;
+import com.example.demo.repositories.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class DemoApplicationTests {
+	private final CategorieRepository categorierepo;
+
+	private final ProduitRepository produitr;
+
+	@Autowired
+	public  DemoApplicationTests(CategorieRepository categorierepo,ProduitRepository produitr){
+		this.categorierepo=categorierepo;
+		this.produitr=produitr;
+
+	}
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	public void testCreateCategorie(){
+		Timestamp t=new Timestamp(System.currentTimeMillis());
+
+		Categories c1= new Categories(1L,"test2",50,t,t,null);
+
+		categorierepo.save(c1);
+		assertThat(categorierepo.save(c1)).isNotNull();
+
+	}
+	@Test
+	public void getAllcategoriesTest(){
+		List<Categories>list=(List<Categories>) categorierepo.findAll();
+		assertThat(list).size().isGreaterThan(0);
+		System.out.println("list %d:"+list.size());
+
+	}
+	@Test
+	void singleCategory () {
+		Categories category  = categorierepo.findById(1L).get() ;
+		assertEquals(100 , category.getQT());
+
+	}
+	@Test
+	public void testCreateProduit(){
+		Timestamp t=new Timestamp(System.currentTimeMillis());
+		long id = 32;
+		Optional<Categories> optCat = categorierepo.findById(id);
+		Categories cat = optCat.get() ;
+		Produit p= new Produit(1L,"test2",50,true, t,t,"unknown.jpg",cat);
+
+
+		assertThat(produitr.save(p)).isNotNull();
+
+	}
+
+
+
+
+}
